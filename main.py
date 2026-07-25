e60b57deb4d7319d39fc0c29715cef503005cd1a
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2026-07-26 02:04:47 +0700

    pipile AI succes intrgration

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 PROJECT_ROOT = Path(__file__).resolve().parent
 
 load_dotenv(
-    PROJECT_ROOT / ".env"
+    Path(".env")
 )
 
 if str(PROJECT_ROOT) not in sys.path:
